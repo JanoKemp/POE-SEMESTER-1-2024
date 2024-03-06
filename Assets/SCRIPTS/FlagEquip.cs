b430379e5cf6de116bf708d9d8a067f273d6d88a
JanoKemp <112688104+JanoKemp@users.noreply.github.com>
2024-03-06 20:07:17 +0200

    added flag and created pickup script

---
+++ Assets/SCRIPTS/FlagEquip.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlagEquip : MonoBehaviour
+{
+    public GameObject Flag;//ref to flag object
+    public Transform FlagHold;//ref to empty gameObject that is set as a child to the player and enemy because this is where the flag will be attached when picked up
+    public float inPickUpRange = 2f;
+    void Start()
+    {
+        Flag.GetComponent<Rigidbody>().isKinematic = true;//get rigidbody komponent attached to the flag and set isKinematic to true meaning no physics will have an effect of the flag
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
